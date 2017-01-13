4caf1b6897e330d7cbe43471b88669b40afdbcff
Nishant Thapa <nishthapa@gmail.com>
2017-01-13 14:19:05 +0530

    Updated code and added Documentation

--- ARDUINO/Brushless_Motor_Slider_Throttle.ino
+++ ARDUINO/Brushless_Motor_Slider_Throttle.ino
@@ -1,53 +1,33 @@
 #include<SoftwareSerial.h>
 #include<Servo.h>
 
-SoftwareSerial bluetooth(9,10);
-Servo esc;
+SoftwareSerial bluetooth(9,10);               //creating a bluetooth object for communication
+Servo esc;                                    //creating Servo object for Electronic Speed Controller
 
-char RAW_DATA[4];
-//char RAW_DATA;
-String THROTTLE_STR;
+char RAW_DATA[4];                             //to read raw data (character from bytes) from the bluetooth stream
+String THROTTLE_STR;                          //to represent throttle in string after reading from bluetooth stream
 int i=0,THROTTLE_INT;
 
 void setup()
 {
   Serial.begin(9600);
-  bluetooth.begin(9600);
-  esc.attach(3);
-  // put your setup code here, to run once:
-
+  bluetooth.begin(9600);                      //begin bluetooth communication
+  esc.attach(3);                              //declare that the Electronic Speed Controller is attached to Arduino pin 3
 }
 
 void loop()
 {
-  THROTTLE_STR = "@@@@@@";
+  THROTTLE_STR = "@@@@@@";                    //for filtering noise
   if(bluetooth.available())
   {
     for(i=0;i<=3;i++)
     {
-      RAW_DATA[i] = bluetooth.read();
-      //RAW_DATA = (char)bluetooth.read();
-      delay(1);
+      RAW_DATA[i] = bluetooth.read();         //read data from bluetooth stream
+      delay(1);                               //give some time for finish reading
     }
   }
-  THROTTLE_STR = (String)RAW_DATA;
-  //THROTTLE_STR = THROTTLE_STR.substring(0,5);
-  //STAR = THROTTLE_STR.indexOf("*");
-  //EXCLA = THROTTLE_STR.indexOf("!");
-  //RESULTANT_THROTTLE = THROTTLE_STR.substring((STAR + 1),(EXCLA));
-  //THROTTLE_INT = RESULTANT_THROTTLE.toInt();
-  //THROTTLE_STR = (String)bluetooth.read();
-  //}
-  //else
-  //{
-    //THROTTLE_INT = 0;
-  //}
-  THROTTLE_INT = THROTTLE_STR.toInt();
-  //THROTTLE_INT = map(THROTTLE_INT,0,9,1000,2000);
-  //Serial.println(THROTTLE_INT);
-  Serial.println(THROTTLE_INT);
-  esc.writeMicroseconds(THROTTLE_INT);
-  //delay(100);
-  // put your main code here, to run repeatedly:
-
+  THROTTLE_STR = (String)RAW_DATA;            //convert read data to string
+  THROTTLE_INT = THROTTLE_STR.toInt();        //convert string to int for implementing Servo write
+  Serial.println(THROTTLE_INT);               //print for debugging on serial monitor
+  esc.writeMicroseconds(THROTTLE_INT);        //finally send throttle signal to the Electronic Speed Controller
 }
